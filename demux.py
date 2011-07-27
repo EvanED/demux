edf200ff4e8c02423abb2aa2cfc3cc112925980a
Evan Driscoll <driscoll@cs.wisc.edu>
2011-07-26 22:48:00 -0500

    Looks like significant changes but it's been too long

--- demux.py
+++ demux.py
@@ -1,38 +1,46 @@
-#!/u/d/r/driscoll/bin/python
+#!/usr/bin/env python
 
 import sys
+
+def shift(l):
+    return l.pop(0)
+    
 
 def debug(m):
     sys.stderr.write(str(m))
     pass
 
-num_matches = 0
-num_differ = 0
 
-def demux(pattern, matching, differing, inputs):
+def demux(specs, inputs):
     import re
-    pattern = re.compile(pattern)
+    # specs comes in as a list of (string, string) tuples. Convert the first
+    # string to an actual regex object and the second to the corresponding
+    # file object.
+    specs = [ (re.compile(pattern), open_or_std(filename, 'w'))
+              for (pattern, filename) in specs ]
 
-    global num_matches
-    global num_differ
-
-    for input in inputs:
-        debug("Processing %s\n" % input.name)
-        for line in input.readlines():
-            line = line.strip()
-            if re.search(pattern, line):
-                debug('+ ')
-                matching.write(line)
-                matching.write('\n')
-                num_matches += 1
-            else:
-                debug('- ')
-                differing.write(line)
-                differing.write('\n')
-                num_differ += 1
+    for input_name in inputs:
+        debug("Processing %s\n" % input_name)
+        with open_or_std(input_name, 'r') as input:
+            for line in input.readlines():
+                line = line.strip()
+                if line == '':
+                    continue
+                match_no = 0
+                for (pattern, file) in specs:
+                    if re.search(pattern, line):
+                        debug('%s ' % (str(match_no),))
+                        file.write(line)
+                        file.write('\n')
+                        break
+                    match_no += 1
+                else:
+                    assert False
+                    
     
 
 def open_or_std(filename, mode):
+    #debug('Opening %s as %s\n' % (filename, mode))
     if filename == '-':
         if mode == 'r':
             return sys.stdin
@@ -43,30 +51,65 @@ def open_or_std(filename, mode):
         return open(filename, mode)
 
 def main():
-    if len(sys.argv) <5:
-        sys.stderr.write('RTFM\n')
-        exit(1)
-
     argv = sys.argv
-    pattern = argv[1]
-    matching_out = argv[2]
-    differing_out = argv[3]
-    input = argv[4:]
 
-    debug('Pattern:   %s\n' % pattern)
-    debug('Matching:  %s\n' % matching_out)
-    debug('Differing: %s\n' % differing_out)
-    debug('Inputs:    %s\n' % input)
+    # A spec is a (regex, filename) pair
+    specs = []
 
-    demux(pattern,
-          open_or_std(matching_out, 'w'),
-          open_or_std(differing_out, 'w'),
-          [open_or_std(file, 'r') for file in input])
+    # Command line syntax is one of the following:
+    #   demux [-e pattern filename]* [-d filename] -- [filename]*
+    #   demux -f specfile [-d filename] -- [filename]*
+
+    shift(argv) # kill the script name
+    if argv[0] == '-e':
+        # Parse the specs from the command line.
+        spec_no = 0
+        while len(argv) > 0 and argv[0] == '-e':
+            shift(argv) # kill the -e
+            pattern = shift(argv)
+            file = shift(argv)
+            specs.append( (pattern,file) )
+            debug('Spec %s: %s <- %s\n' % (str(spec_no), file, pattern) )
+            spec_no += 1
+    else:
+        # Parse the specs from a file
+        assert shift(argv) == '-f'
+        specs_file = shift(argv)
+        with open_or_std(specs_file, 'r') as file:
+            spec_no = 0
+            for line in file.readlines():
+                line = line.strip()
+                split = line.split('<-', 1)
+                if len(split) != 2:
+                    continue
+                [outfile, pattern] = split
+                outfile = outfile.strip()
+                pattern = pattern.strip()
+                specs.append( (pattern, outfile) )
+                debug('Spec %s: %s <- %s\n' % (str(spec_no), outfile, pattern) )
+                spec_no += 1
+
+    if len(argv) == 0 or argv[0] != '-d':
+        default_file = '-'
+    else:
+        assert shift(argv) == '-d'
+        default_file = shift(argv)
+
+    specs.append( ('.*', default_file) )
+
+    # Okay, we have all information about output files. Now get inputs.
+    if len(argv) > 0:
+        assert shift(argv) == '--'
+        inputs = argv
+    else:
+        # No inputs specified; read from stdin
+        inputs = ['-']
+        
+    demux(specs, inputs)
 
 
-    debug('\n# matching lines:  %s\n' % str(num_matches))
-    debug('# differing lines: %s\n' % str(num_differ))
 
 if __name__ == '__main__':
     main()
+    debug('\n')
 
